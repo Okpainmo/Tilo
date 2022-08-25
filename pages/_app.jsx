63cd08108d25207bff04c8f4ca5f14cf1ec33d54
Okpainmo <okpainmoandrew@gmail.com>
2022-08-25 12:09:25 +0100

    refactor(_app, _document): ...updates

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css';
+// import NavbarContext from '../contexts/NavbarContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="bg--default text--colors_default nunito-sans">
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
