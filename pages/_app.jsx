5cecba4ba48c75ddd7d5c1bf44a45c54c7fd1564
Okpainmo <okpainmoandrew@gmail.com>
2022-09-04 19:23:34 +0100

    refactor(_document, _app): updated google fonts + RTK based modifications

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,19 +6,24 @@ import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 // import NavbarContext from '../contexts/NavbarContext';
 
+import { Provider } from 'react-redux';
+import store from '../store';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <main className="bg--default text--colors_default nunito-sans">
-      <ToastContainer
-        position="top-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-      />
-      <Component {...pageProps} />
-    </main>
+    <Provider store={store}>
+      <main className="bg--default text--colors_default nunito-sans">
+        <ToastContainer
+          position="top-left"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+        />
+        <Component {...pageProps} />
+      </main>
+    </Provider>
   );
 }
 
